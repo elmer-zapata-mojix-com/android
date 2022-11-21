3142d817843b67ff2c7e12f05a159df9ae575e23
Elmer Zapata <elmer.765@gmail.com>
2022-11-21 17:46:13 -0400

    add simple example

--- appium-android/src/main/java/org/example/MainIOS.java
+++ appium-android/src/main/java/org/example/MainIOS.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.interactions.PointerInput;
@@ -36,31 +37,31 @@ public class MainIOS {
         capabilities.setCapability("appium:app", "/Users/elmerzapata/Downloads/MyRNDemoApp.app");
 
 
-        capabilities.setCapability("noReset", true);
+//        capabilities.setCapability("noReset", true);
 
         IOSDriver driver = new IOSDriver(new URL("http://localhost:4723/"), capabilities);
 
         driver.manage().timeouts().implicitlyWait( Duration.of(5, TimeUnit.SECONDS.toChronoUnit()));
 
 
-//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"Notificaciones, pestaña 5 de 6\"]")).click();
-        Dimension dimension = driver.manage().window().getSize();
-
-        PointerInput FINGER = new PointerInput(TOUCH, "finger");
-        Point start = new Point((int)(dimension.width*0.5), (int)(dimension.height*0.9));
-        Point end = new Point((int)(dimension.width*0.2), (int)(dimension.height*0.1));
-        Sequence swipe = new Sequence(FINGER, 1)
-                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
-                .addAction(FINGER.createPointerDown(LEFT.asArg()))
-                .addAction(FINGER.createPointerMove(ofMillis(2000), viewport(), end.getX(), end.getY()))
-                .addAction(FINGER.createPointerUp(LEFT.asArg()));
-        driver.perform(singletonList(swipe));
-        Sequence swipe2 = new Sequence(FINGER, 1)
-                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
-                .addAction(FINGER.createPointerDown(LEFT.asArg()))
-                .addAction(FINGER.createPointerMove(ofMillis(2000), viewport(), end.getX(), end.getY()))
-                .addAction(FINGER.createPointerUp(LEFT.asArg()));
-        driver.perform(singletonList(swipe2));
+        driver.findElement(By.id("Sauce Labs Bike Light")).click();
+//        Dimension dimension = driver.manage().window().getSize();
+//
+//        PointerInput FINGER = new PointerInput(TOUCH, "finger");
+//        Point start = new Point((int)(dimension.width*0.5), (int)(dimension.height*0.9));
+//        Point end = new Point((int)(dimension.width*0.2), (int)(dimension.height*0.1));
+//        Sequence swipe = new Sequence(FINGER, 1)
+//                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
+//                .addAction(FINGER.createPointerDown(LEFT.asArg()))
+//                .addAction(FINGER.createPointerMove(ofMillis(2000), viewport(), end.getX(), end.getY()))
+//                .addAction(FINGER.createPointerUp(LEFT.asArg()));
+//        driver.perform(singletonList(swipe));
+//        Sequence swipe2 = new Sequence(FINGER, 1)
+//                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
+//                .addAction(FINGER.createPointerDown(LEFT.asArg()))
+//                .addAction(FINGER.createPointerMove(ofMillis(2000), viewport(), end.getX(), end.getY()))
+//                .addAction(FINGER.createPointerUp(LEFT.asArg()));
+//        driver.perform(singletonList(swipe2));
 
     }
 
